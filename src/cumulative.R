fb122aa8e930db96d431c03bc54a3ff7af135f55
TurtlePU <sokolov.p64@gmail.com>
2021-06-10 09:37:04 +0300

    + intersect & merge output

---
+++ src/cumulative.R
@@ -0,0 +1,26 @@
+library(ggplot2)
+library(ChIPseeker)
+library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+library(clusterProfiler)
+
+for (kind in c('merge.hg19', 'intersect.G4')) {
+    name <- paste0('H3K27ac_A549.', kind)
+    fname <- paste0('data/', name, '.bed')
+
+    bed_df <- read.delim(fname, as.is = TRUE, header = FALSE)
+    colnames(bed_df) <- c('chrom', 'start', 'end')
+    bed_df$len <- bed_df$end - bed_df$start
+
+    subtitle <- sprintf('Number of peaks = %s', nrow(bed_df))
+    pname <- paste('len_hist', name, 'pdf', sep = '.')
+
+    ggplot(bed_df) + aes(x = len) + geom_histogram() +
+        ggtitle(name, subtitle = subtitle) + theme_bw()
+    ggsave(pname, path = 'img')
+
+    peakAnno <- annotatePeak(fname, tssRegion=c(-3000, 3000),
+        TxDb=TxDb.Hsapiens.UCSC.hg19.knownGene, annoDb="org.Hs.eg.db")
+    png(paste0('img/chip_seeker.', name, '.plotAnnoPie.png'))
+    plotAnnoPie(peakAnno)
+    dev.off()
+}
